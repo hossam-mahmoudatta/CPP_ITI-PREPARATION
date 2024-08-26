0529a0acc04ae3f68c1e59a4e5436d36b7368b09
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-08-26 17:21:33 +0300

    creating a class

--- PRACTICE01_C++/CARS_CLASS_HDR.h
+++ PRACTICE01_C++/CARS_CLASS_HDR.h
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+class Car {
+  private:
+    int model;
+    string maker;
+    string color;
+}
--- PRACTICE01_C++/CARS_CLASS_SRC.cpp
+++ PRACTICE01_C++/CARS_CLASS_SRC.cpp
@@ -1,13 +1,11 @@
 
-#include <iostream>
-#include <vector>
-#include <string>
 
-using namespace std;
+
+
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!\n"};
 
     for (const string& word : msg)
     {
