74515dd1e852595aadb28c99f35dbfe1a9fcad82
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-09-19 21:31:06 +0300

    adding additional member functions

--- CH01_EX01_OOP_MAHARATECH/include/Complex/Complex.h
+++ CH01_EX01_OOP_MAHARATECH/include/Complex/Complex.h
@@ -8,22 +8,23 @@ private:
     float imag;
 
 public:
-    // Constructor
-    // Complex(double real, double imag);
+    // Constructors
+    Complex();                  // No Parameter Constructor
+    Complex(float r);           // 1 Parameter Constructor
+    Complex(float r, float i);  // 2 Parameters Constructor
+    ~Complex();                 // Destructor
+    void setComplex(float r, float, i);
 
-    void setReal(float);  // Set the real part
+    // Setter and Getter Functions
+    void    setReal(float);     // Set the real part
+    void    setImag(float);     // Set the imaginary part
+    float   getReal();          // Get the real part
+    float   getImag();          // Get the imaginary part
 
-    void setImag(float);  // Set the imaginary part
-
-    float getReal();      // Get the real part
-
-    float getImag();      // Get the imaginary part
-
-    Complex add(Complex c);   // Add two complex numbers
-
+    // Member Functions
+    Complex add(Complex c);     // Add two complex numbers
     Complex sub(Complex c);    // Subtract two complex numbers
-
-    void print();         // Print the complex number
+    void    print();            // Print the complex number
 
     // Function to add two Complex numbers
     // Complex add(const Complex& other) const;
