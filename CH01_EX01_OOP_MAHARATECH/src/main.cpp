284f38630e102018bd45e0121debc8cdb1334a0b
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-09-19 21:18:14 +0300

    some changes in the Complex Class

--- CH01_EX01_OOP_MAHARATECH/src/main.cpp
+++ CH01_EX01_OOP_MAHARATECH/src/main.cpp
@@ -4,11 +4,22 @@
 using namespace std;
 
 int main() {
-  Complex c1;
+  Complex c1, c2, c3;
+
   c1.setReal(6);
-  c1.setImag(-3);
+  c1.setImag(3);
+
+  c2.setReal(3);
+  c2.setImag(4);
 
   c1.print();
+  c2.print();
+
+  c3 = c1.add(c2);
+  c3.print();
+
+  c3 = c1.sub(c2);
+  c3.print();
 
   return 0;
 }
